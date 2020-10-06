709234e6590c71e9204e05f59f4455f0f6f772a4
Dai Dong <vandaidong@gmail.com>
2020-10-05 22:57:10 -0400

    commit

--- app.R
+++ app.R
@@ -10,7 +10,7 @@
 
 ## Fix Date: the upper bound needs to be larger than the lower bound
 
-setwd("C:\\Users\\VanDai Dong\\Downloads\\FinalProject")
+# Load the libraries
 library(shiny)
 library(tidyverse)
 library(ggplot2)
@@ -19,7 +19,7 @@ library(plotly)
 library(scales)
 library(xtable)
 
-
+# Call the data.R file created before
 source('data.R')
 
 # Assign each country with a distinct color
@@ -31,9 +31,8 @@ stat_choices <- c('Number of Confirmed Cases' = 'ConfirmNum',
                   'Number of Recovered Cases' = 'RecoverNum')
 
 
-# Give each continent a color
+# Assign each continent a color
 names(cont_color) <- sort(cont_choices$Continent)
-
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -41,9 +40,8 @@ ui <- fluidPage(
     # Application title
     titlePanel("Coronavirus Statistics"),
 
-    #Sidebar with a slider input for number of bins
     sidebarPanel(
-        # put your input controls here!
+        # The first tab: Time Series Plot
         conditionalPanel(condition = "input.tabs == 'Time Series Plot'",
                          selectInput(inputId="stat", label="Select a Category",
                                      choices=stat_choices),
@@ -57,7 +55,7 @@ ui <- fluidPage(
                          dateRangeInput(inputId = "dates", label="Pick Date Range",
                                         start=min(virus$Date), end=max(virus$Date))
                          ),
-        
+        # The second tab: Bubble Plot
         conditionalPanel(condition = "input.tabs == 'Bubble Plot'",
                          selectInput(inputId="continent", label="Select a Continent",
                                      choices=cont_choices$Continent,
@@ -71,7 +69,6 @@ ui <- fluidPage(
       
     ),
 
-        # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(id='tabs',
                         tabPanel("Time Series Plot",  plotlyOutput(outputId = "timePlot")),
@@ -82,80 +79,87 @@ ui <- fluidPage(
 
 
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output, session) {
-
-    output$timePlot <- renderPlotly({
-        
-        plot1 <- virus %>%
-            filter(Date <= max(ymd(input$dates)),
-                   Date >= min(ymd(input$dates)),
-                   Country.Region %in% input$country)
-    
-        p1 <- ggplot(plot1) +
-            geom_line(aes_string(x='Date', y=input$stat, color='Country.Region')) +
-            geom_point(aes_string(x='Date', y=input$stat, color='Country.Region')) +
-            scale_y_continuous(labels =comma) +
-            scale_color_manual(name = '',
-                               values = country_color[input$country]) +
-            theme_bw() +
-            theme(plot.caption = element_text(size=12),
-                  axis.text = element_text(size = 12))
-        
-        ggplotly(p1) %>%
-          layout(title = paste0('The Time Series Plot of the ', names(stat_choices)[stat_choices==input$stat]),
-                 xaxis=list(
-                 title = 'Date
-                  <br>                                                     Source: JHU CSSE',
-                  titlefont = list(size = 14),
-                  tickfont = list(size = 10)),
-                  margin = list(l = 50, r = 50, t = 60, b = 60),
-                  yaxis = list(title = names(stat_choices)[stat_choices==input$stat],
-                              titlefont = list(size = 14),
-                              tickfont = list(size = 10)))
-        
-    }) 
-    
-    output$bubblePlot <- renderPlotly({
-        plot2 <- virus %>%
-            filter(Continent %in% input$continent)
-        
-        max_xaxis <- max(plot2$DeathNum)
-        max_yaxis <- max(plot2$RecoverNum)
-        
-        plot2 <- plot2 %>%
-          filter(Date == input$date)
-        
-        
-        p2 <- ggplot(plot2, aes(label = Country.Region)) +
-          geom_point(aes_string(x='DeathNum', y='RecoverNum', 
-                                size='ConfirmNum', color='Continent'),
-                     alpha = 0.5) +
+  
+  # First plot: Time plot
+  output$timePlot <- renderPlotly({
+      # Select only the range that a user chose
+      plot1 <- virus %>%
+          filter(Date <= max(ymd(input$dates)),
+                 Date >= min(ymd(input$dates)),
+                 Country.Region %in% input$country)
+      
+      # Create a plot
+      p1 <- ggplot(plot1) +
+          geom_line(aes_string(x='Date', y=input$stat, color='Country.Region')) +
+          geom_point(aes_string(x='Date', y=input$stat, color='Country.Region')) +
+          scale_y_continuous(labels =comma) +
           scale_color_manual(name = '',
-                             values = cont_color[input$continent]) +
-          scale_x_continuous(limits = c(0, max_xaxis),
-                             labels = comma) +
-          scale_y_continuous(limits = c(0, max_yaxis),
-                             labels = comma) +
-          theme_bw()
-        
-        
-        ggplotly(p2) %>%
-          layout(title = paste0('The relationship between the number of Death Cases and the Recovered Cases by Continent'),
-                 titlefont=list(size =16),
-                 xaxis=list(
-                   title = '<br><br><br><br>Number of Death Cases
-                   <br>                                                     Source: JHU CSSE
-                   
-                             * Each bubble represents for a country
-                             ** The bubble\'s size represents for the number of confirmed Cases',
-                   titlefont = list(size = 14),
-                   tickfont = list(size = 10)),
-                 yaxis = list(title = "Number of Recovered Cases",
-                              titlefont = list(size = 14),
-                              tickfont = list(size = 10)))
-        
-    })
+                             values = country_color[input$country]) +
+          theme_bw() +
+          theme(plot.caption = element_text(size=12),
+                axis.text = element_text(size = 12))
+      
+      # Create a plotly plot
+      ggplotly(p1) %>%
+        layout(title = paste0('The Time Series Plot of the ', names(stat_choices)[stat_choices==input$stat]),
+               xaxis=list(
+               title = 'Date
+                <br>                                                     Source: JHU CSSE',
+                titlefont = list(size = 14),
+                tickfont = list(size = 10)),
+                margin = list(l = 50, r = 50, t = 60, b = 60),
+                yaxis = list(title = names(stat_choices)[stat_choices==input$stat],
+                            titlefont = list(size = 14),
+                            tickfont = list(size = 10)))
+      
+  }) 
+  
+  # The second plot: Bubble plot
+  output$bubblePlot <- renderPlotly({
+      # Select only continent that a use picked
+      plot2 <- virus %>%
+          filter(Continent %in% input$continent)
+      
+      # Specify the maximum of the x- and y-axis
+      max_xaxis <- max(plot2$DeathNum)
+      max_yaxis <- max(plot2$RecoverNum)
+      
+      # Select the beginning date that a use chose
+      plot2 <- plot2 %>%
+        filter(Date == input$date)
+      
+      # Create a plot
+      p2 <- ggplot(plot2, aes(label = Country.Region)) +
+        geom_point(aes_string(x='DeathNum', y='RecoverNum', 
+                              size='ConfirmNum', color='Continent'),
+                   alpha = 0.5) +
+        scale_color_manual(name = '',
+                           values = cont_color[input$continent]) +
+        scale_x_continuous(limits = c(0, max_xaxis),
+                           labels = comma) +
+        scale_y_continuous(limits = c(0, max_yaxis),
+                           labels = comma) +
+        theme_bw()
+      
+      # Create a plotly plot
+      ggplotly(p2) %>%
+        layout(title = paste0('The relationship between the number of Death Cases and the Recovered Cases by Continent'),
+               titlefont=list(size =16),
+               xaxis=list(
+                 title = '<br><br><br><br>Number of Death Cases
+                 <br>                                                     Source: JHU CSSE
+                 
+                           * Each bubble represents for a country
+                           ** The bubble\'s size represents for the number of confirmed Cases',
+                 titlefont = list(size = 14),
+                 tickfont = list(size = 10)),
+               yaxis = list(title = "Number of Recovered Cases",
+                            titlefont = list(size = 14),
+                            tickfont = list(size = 10)))
+      
+  })
     
 }
 
--- data.R
+++ data.R
@@ -1,6 +1,9 @@
+# Load libraries
 library(tidyverse)
 library(lubridate)
 
+# Create a function that change the wide to tall format, change the format 
+# of the Date variable, and calculate the culmulative cases
 dfTransform <- function(df) {
   df <- df%>%
     select(-Province.State, -Lat, -Long) %>%
@@ -15,39 +18,52 @@ dfTransform <- function(df) {
   return(df)
 }
 
+####################################
 # Confirm Dataset
+####################################
+# Read in the data set that retrieved from John Hopskins University
 global_confirmed <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", 
                              sep=",", 
                              header=TRUE)
+# Transform the data frame
 global_confirmed <- dfTransform(global_confirmed)
+# Rename the TotalCases variable to ConfirmNum variable
 global_confirmed <- global_confirmed %>%
   rename(ConfirmNum = TotalCases)
 
+####################################
 # Death Dataset
+####################################
+# Read in the data set that retrieved from John Hopskins University
 global_deaths <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
                           sep=",",
                           header=TRUE)
+# Transform the data frame
 global_deaths <- dfTransform(global_deaths)
+# Rename the TotalCases variable to DeathNum variable
 global_deaths <- global_deaths %>%
   rename(DeathNum = TotalCases)
 
-
+####################################
 # Recover Dataset
+####################################
+# Read in the data set that retrieved from John Hopskins University
 global_recovers <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv",
                             sep=',',
                             header=TRUE)
-
+# Transform the data frame
 global_recovers <- dfTransform(global_recovers)
+# Rename the TotalCases variable to RecoverNum variable
 global_recovers <- global_recovers %>%
   rename(RecoverNum = TotalCases)
 
 
-# Merge 3 dataset together
+# Merge 3 dataframes together
 virus <- merge(global_confirmed, global_deaths, by=c("Country.Region", "Date"))
 virus <- merge(virus, global_recovers, by=c("Country.Region", "Date"))
 
 
-# Calculate World's Statistics
+# Calculate the statistics for the whole world
 world <- virus %>%
   mutate(Country.Region = "World",
          Date = Date) %>%
@@ -57,7 +73,7 @@ world <- virus %>%
             RecoverNum = sum(RecoverNum, na.rm = TRUE)) %>%
   as.data.frame(world)
 
-# Merge 2 dataframe together
+# Merge 2 dataframes together
 virus <- rbind(virus, world)
 virus <- virus %>%
   mutate(Country.Region = as.character(Country.Region))
@@ -72,7 +88,7 @@ continent <- continent %>%
 # Join 2 datasets together
 virus <- left_join(virus, continent, by=c("Country.Region" =  "Country"))
 
-# FInd out which country is not assigned to a continent
+# Find out which country is not assigned to a continent
 df <- virus[is.na(virus$Continent),] %>%
   group_by(Country.Region) %>%
   summarise(N = n())
@@ -110,9 +126,7 @@ colors_df <- read.csv('https://raw.githubusercontent.com/codebrainz/color-names/
 colors <- colors_df[c(1:nrow(country)),]
 country_color <- as.character(colors$V3)
 
-# List of continent
-
-# List of category for plot 2
+# List of categories used plot 2
 cont_choices <- virus %>%
   filter(Continent %in% c('Asia', 'Africa', 'North America', 'South America', 
                           'Europe', 'Oceania', 'Asia/Europe')) %>%
